4630285c1c449b50457bf73fbbd0722526e062e4
湯　不園 <ellentby@163.com>
2016-10-27 11:53:43 +0900

    delete temp files

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var path = require("path");
-var md = require('markdown-it')
 var marked = require('marked');
 
 app.set('views', path.join(__dirname, 'views'));
